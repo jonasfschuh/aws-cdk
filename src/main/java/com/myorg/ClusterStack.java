fb5fa4ae556f9af342b3aa07e6721bb2a7864dab
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-14 19:02:41 -0300

    updated ClusterStack

--- src/main/java/com/myorg/ClusterStack.java
+++ src/main/java/com/myorg/ClusterStack.java
@@ -2,23 +2,21 @@ package com.myorg;
 
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecs.Cluster;
 import software.constructs.Construct;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class ClusterStack extends Stack {
-    public ClusterStack(final Construct scope, final String id) {
-        this(scope, id, null);
+    public ClusterStack(final Construct scope, final String id, Vpc vpc) {
+        this(scope, id, null, vpc);
     }
 
-    public ClusterStack(final Construct scope, final String id, final StackProps props) {
+    public ClusterStack(final Construct scope, final String id, final StackProps props, Vpc vpc) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
-
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "AwsCdkQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Cluster.Builder.create(this, id)
+            .clusterName("cluster-01")
+            .vpc(vpc)
+            .build();
     }
 }
