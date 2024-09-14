d4ae9ae685874a23a5b9ac45cc87e1306062c81c
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-14 19:28:43 -0300

    created Service01Stack

---
+++ src/main/java/com/myorg/Service01Stack.java
@@ -0,0 +1,46 @@
+package com.myorg;
+
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.constructs.Construct;
+
+public class Service01Stack extends Stack {
+    public Service01Stack(final Construct scope, final String id, Cluster cluster) {
+        this(scope, id, null, clusterStack);
+    }
+
+    public Service01Stack(final Construct scope, final String id, final StackProps props, Cluster cluster) {
+        super(scope, id, props);
+
+        ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB01")
+                .serviceName("service-01")
+                .cluster(cluster)
+                .cpu(512)
+                .memoryLimitMiB(1024)
+                .desiredCount(2)
+                .listenerPort(8080)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .containerName("aws_restful")
+                                .image(ContainerImage.fromRegistry("jonasschuh/aws_restful:1.0.0"))
+                                .containerPort(8080)
+                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                                        .logGroup(LogGroup.Builder.create(this, "Service01LogGroup")
+                                                .logGroupName("Service01")
+                                                .removalPolicy(RemovalPolicy.DESTROY)
+                                                .build())
+                                        .streamPrefix("Service01")
+                                        .build()))
+                                .build())
+                .publicLoadBalancer(true)
+                .build();
+    }
+}
