79394cff008ad318a979019ce8f138373ff4e8fd
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-15 09:31:25 -0300

    created RdsStack

--- src/main/java/com/myorg/RdsStack.java
+++ src/main/java/com/myorg/RdsStack.java
@@ -4,6 +4,9 @@ import software.amazon.awscdk.CfnParameter;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.rds.DatabaseInstanceEngine;
+import software.amazon.awscdk.services.rds.MySqlInstanceEngineProps;
+import software.amazon.awscdk.services.rds.MysqlEngineVersion;
 import software.constructs.Construct;
 
 public class RdsStack extends Stack {
@@ -20,5 +23,10 @@ public class RdsStack extends Stack {
                 .build();
         ISecurityGroup iSecurityGroup = SecurityGroup.fromSecurityGroupId(this, id, vpc.getVpcDefaultSecurityGroup());
         iSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3306));
+
+        DatabaseInstance.Builder.create(this, "RdsInstance")
+                .engine(DatabaseInstanceEngine.mysql(MySqlInstanceEngineProps.builder()
+                        .version(MysqlEngineVersion.VER_8_0_19)
+                        .build()))
     }
 }
