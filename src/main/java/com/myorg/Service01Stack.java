7974bd941d35f7381eecc938b1c65b165f4465ef
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-15 10:35:12 -0300

    updated RDS params

--- src/main/java/com/myorg/Service01Stack.java
+++ src/main/java/com/myorg/Service01Stack.java
@@ -45,6 +45,7 @@ public class Service01Stack extends Stack {
                                                 .build())
                                         .streamPrefix("Service01")
                                         .build()))
+                                .environment(envVariables)
                                 .build())
                 .publicLoadBalancer(true)
                 .build();
