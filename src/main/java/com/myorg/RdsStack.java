68f1edc968523e48679cebe7b12edb1ba1cd3bf1
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-15 09:19:20 -0300

    created RdsStack

--- src/main/java/com/myorg/RdsStack.java
+++ src/main/java/com/myorg/RdsStack.java
@@ -3,6 +3,10 @@ package com.myorg;
 import software.amazon.awscdk.CfnParameter;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.ISecurityGroup;
+import software.amazon.awscdk.services.ec2.Peer;
+import software.amazon.awscdk.services.ec2.Port;
+import software.amazon.awscdk.services.ec2.SecurityGroup;
 import software.constructs.Construct;
 
 public class RdsStack extends Stack {
@@ -17,6 +21,7 @@ public class RdsStack extends Stack {
                 .type("String")
                 .description("The password for the database")
                 .build();
-
+        ISecurityGroup iSecurityGroup = SecurityGroup.fromSecurityGroupId(this, id, vpc.getVpcDefaultSecurityGroup());
+        iSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3306));
     }
 }
