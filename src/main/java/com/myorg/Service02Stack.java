b2d6665c3d541bebdc559a22743098926969af27
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-22 11:45:38 -0300

    update Service02Stack

--- src/main/java/com/myorg/Service02Stack.java
+++ src/main/java/com/myorg/Service02Stack.java
@@ -59,8 +59,8 @@ public class Service02Stack extends Stack {
                 .listenerPort(9090)
                 .taskImageOptions(
                         ApplicationLoadBalancedTaskImageOptions.builder()
-                                .containerName("aws_project02")
-                                .image(ContainerImage.fromRegistry("jonasschuh/curso_aws_project02:1.5.0"))
+                                .containerName("aws_consumer")
+                                .image(ContainerImage.fromRegistry("jonasschuh/aws_consumer:1.0.0"))
                                 .containerPort(9090)
                                 .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
                                         .logGroup(LogGroup.Builder.create(this, "Service02LogGroup")
