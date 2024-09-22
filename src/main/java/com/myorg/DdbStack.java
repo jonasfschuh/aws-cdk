e90ddbbe803bf7d2c98a86bdfb3d362e95893629
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-22 10:21:40 -0300

    add DdbStack

---
+++ src/main/java/com/myorg/DdbStack.java
@@ -0,0 +1,57 @@
+package com.myorg;
+
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
+import software.amazon.awscdk.services.dynamodb.BillingMode;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.constructs.Construct;
+
+public class DdbStack extends Stack {
+    private final Table productEventsDdb;
+
+    public DdbStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public DdbStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        productEventsDdb = Table.Builder.create(this, "ProductEventsDb")
+                .tableName("product-events")
+                .billingMode(BillingMode.PAY_PER_REQUEST)
+                .partitionKey(Attribute.builder()
+                        .name("pk")
+                        .type(AttributeType.STRING)
+                        .build())
+                .sortKey(Attribute.builder()
+                        .name("sk")
+                        .type(AttributeType.STRING)
+                        .build())
+                .timeToLiveAttribute("ttl")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+    }
+
+    public Table getProductEventsDdb() {
+        return productEventsDdb;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
