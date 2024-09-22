698467600053647033ea5d79e9f63e53a697b0de
Jonas Fernando Schuh <jonasschuh@hotmail.com>
2024-09-22 10:20:45 -0300

    update AwsCdkApp

--- src/main/java/com/myorg/AwsCdkApp.java
+++ src/main/java/com/myorg/AwsCdkApp.java
@@ -21,11 +21,23 @@ public class AwsCdkApp {
 
         SnsStack snsStack = new SnsStack(app, "Sns");
 
+        InvoiceAppStack invoiceAppStack = new InvoiceAppStack(app, "InvoiceApp");
+
         Service01Stack service01Stack = new Service01Stack(app, "Service01",
-                clusterStack.getCluster(), snsStack.getProductEventsTopic());
+                clusterStack.getCluster(), snsStack.getProductEventsTopic(),
+                invoiceAppStack.getBucket(), invoiceAppStack.getS3InvoiceQueue());
         service01Stack.addDependency(clusterStack);
         service01Stack.addDependency(rdsStack);
         service01Stack.addDependency(snsStack);
+        service01Stack.addDependency(invoiceAppStack);
+
+        DdbStack ddbStack = new DdbStack(app, "Ddb");
+
+        Service02Stack service02Stack = new Service02Stack(app, "Service02",
+                clusterStack.getCluster(), snsStack.getProductEventsTopic(), ddbStack.getProductEventsDdb());
+        service02Stack.addDependency(clusterStack);
+        service02Stack.addDependency(snsStack);
+        service02Stack.addDependency(ddbStack);
 
         app.synth();
     }
